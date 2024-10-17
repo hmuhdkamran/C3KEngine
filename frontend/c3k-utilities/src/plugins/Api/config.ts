1c117ebcf95c09b261258cbf04a418008ba5b777
H.Muhammad Kamran <hmuhdkamran@gmail.com>
2024-10-17 14:48:21 +0500

    remove unwanted log

--- frontend/c3k-utilities/src/plugins/Api/config.ts
+++ frontend/c3k-utilities/src/plugins/Api/config.ts
@@ -12,7 +12,6 @@ const addProp = (obj: Record<string, string>, propName: string, value: string) =
     Object.defineProperty(obj, propName, {
         enumerable: false,
         get: () => {
-            console.log(`//${window.location.host}${value}, Or //${DEV_API}${value}, Environment: ${isProduction}`)
             return isProduction ? `//${window.location.host}${value}` : `//${DEV_API}${value}`;
         },
     });
