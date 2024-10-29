75f74ab27c78637d01fa444f5597dd7b24016333
SaniyaSaeed <sania.saeedsefam@gmail.com>
2024-10-28 22:33:36 -0700

    Updated sidebar

--- frontend/packages/root/src/layouts/components/auth/AppSidebar.vue
+++ frontend/packages/root/src/layouts/components/auth/AppSidebar.vue
@@ -8,7 +8,11 @@ interface Props {
   showSidebarDropdown: boolean
 }
 
+interface Emit {
+  (e: 'toggleSidebar'): void;
+}
 const props = defineProps<Props>()
+const emit = defineEmits<Emit>()
 const openSections = ref<{ [key: number]: boolean }>({});
 
 const toggleSection = (index: number) => {
@@ -17,17 +21,23 @@ const toggleSection = (index: number) => {
 </script>
 
 <template>
-  <Drawer :isOpen="props.showSidebarDropdown" size="w-80" @toggleDrawer="emit('toggleDrawer')">
+  <Drawer :isOpen="props.showSidebarDropdown" size="w-80" @toggleDrawer="emit('toggleSidebar')">
     <template #header>
-      <div class="flex items-center justify-between px-5 py-4 bg-white border-b border-gray-200 shadow-sm text-indigo-600">
-        <a href="/" class="flex items-center space-x-2">
-          <VNodeRenderer :nodes="config.logo" />
-          <span>{{ config.application }}</span>
-        </a>
+      <div
+        class="w-full flex items-center justify-between px-6 py-2 bg-white border-b border-gray-200 shadow-sm text-gray-700 hover:text-indigo-600 space-x-8">
+        <div>
+          <a href="/" class="flex items-center space-x-2">
+            <VNodeRenderer :nodes="config.logo" />
+            <span>{{ config.application }}</span>
+          </a>
+        </div>
+        <button @click="emit('toggleSidebar')" class="text-gray-400 hover:text-gray-600 focus:outline-none">
+          <span class="icon-[fluent--dismiss-20-filled] h-4 w-4"></span>
+        </button>
       </div>
     </template>
     <div
-      class="fixed inset-y-0 top-28 left-0 w-80 bg-white text-gray-800 border-r border-gray-200 shadow-lg z-50 rounded-tr-lg rounded-br-lg">
+      class="fixed inset-y-0 top-28 left-0 w-80 bg-white text-gray-800 border-r border-gray-200 shadow-lg z-40 rounded-tr-lg rounded-br-lg">
       <div class="flex flex-col h-full">
         <div class="overflow-y-auto flex-grow px-4 py-3">
           <ul class="space-y-2">
@@ -70,36 +80,4 @@ const toggleSection = (index: number) => {
 </template>
 
 <style scoped>
-.slide-enter-active,
-.slide-leave-active {
-  transition: transform 0.4s ease;
-}
-
-.slide-enter-from,
-.slide-leave-to {
-  transform: translateX(-100%);
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease;
-}
-
-.slide-down-enter-active,
-.slide-down-leave-active {
-  transition: max-height 0.3s ease, opacity 0.3s ease;
-}
-
-.slide-down-enter-from,
-.slide-down-leave-to {
-  max-height: 0;
-  opacity: 0;
-}
-
-.slide-down-enter-to,
-.slide-down-leave-from {
-  max-height: 200px;
-  /* Adjust as needed for the content size */
-  opacity: 1;
-}
 </style>
