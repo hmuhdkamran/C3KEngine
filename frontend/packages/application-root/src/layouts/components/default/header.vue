01c07e11346feb7d05ca76f4d07f7d79c08b38a1
H.Muhammad Kamran <hmuhdkamran@gmail.com>
2024-11-05 17:01:47 +0500

    update header

--- frontend/packages/application-root/src/layouts/components/default/header.vue
+++ frontend/packages/application-root/src/layouts/components/default/header.vue
@@ -6,7 +6,7 @@ import { config, VNodeRenderer } from 'c3k-library';
     <nav class="fixed z-50 w-full bg-white top-0 flex flex-wrap items-center justify-between px-1 py-1 shadow-lg">
         <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
             <div class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-700"
+                <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-1 whitespace-nowrap uppercase text-gray-700"
                     href="/">
                     <VNodeRenderer :nodes="config.logo" />{{ config.application }}</a>
                 <button
@@ -15,15 +15,15 @@ import { config, VNodeRenderer } from 'c3k-library';
                     <span class="icon-[fa-solid--bars]"></span>
                 </button>
             </div>
-            <div class="lg:flex flex-grow items-center hidden" id="example-navbar-danger">
+            <div class="lg:flex flex-grow items-center hidden">
                 <ul class="flex flex-col lg:flex-row list-none lg:ml-auto gap-4">
                     <li class="nav-item" v-for="item in config.socialMedia">
-                        <a :href="item.link" class="btn-rounded" target="_blank">
+                        <a :href="item.link" target="_blank">
                             <span :class="item.icon"></span>
                         </a>
                     </li>
                     <li class="nav-item cursor-pointer ">
-                        <RouterLink to="/authentication/login" class="btn-gradient px-4 py-2">
+                        <RouterLink to="/authentication/login" class="btn-outline btn-primary px-4 py-1">
                             Login
                         </RouterLink>
                     </li>
