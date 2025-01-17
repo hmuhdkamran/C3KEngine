c94fa78fa4736696277e121bde6dfb7bcd6c845b
H.Muhammad Kamran <hm.kamran@sefam.com>
2025-01-17 10:00:04 +0500

    Add repository to library

--- frontend/libraries/library/src/service/iservice.ts
+++ frontend/libraries/library/src/service/iservice.ts
@@ -4,3 +4,11 @@ export interface IStoreService {
   exec: <T>(cb: Promise<{}>) => Promise<IPayload<T>>
   request: <T>(method: 'GET' | 'POST' | 'PUT' | 'DELETE', url: string, data?: any, auth?: boolean) => Promise<IPayload<T>>
 }
+
+export interface IRepository<TEntity> {
+  getAll(): Promise<TEntity[]>;
+  getByFilter(filter: string): Promise<TEntity[]>;
+  add(entity: TEntity): Promise<boolean>;
+  update(entity: TEntity): Promise<boolean>;
+  remove(id: string): Promise<boolean>;
+}
---
+++ frontend/libraries/library/src/service/repository.ts
@@ -0,0 +1,56 @@
+import { GlobalConfig, StoreService, type IRepository } from '@/main'
+
+export class RepositoryService<TEntity> extends StoreService implements IRepository<TEntity> {
+  private readonly baseUrl: string
+
+  constructor(baseUrl: string) {
+    super()
+
+    this.baseUrl = `${GlobalConfig.uri.services}${baseUrl}`
+  }
+
+  getAll(): Promise<TEntity[]> {
+    return this.get<TEntity[]>(`${this.baseUrl}`, true)
+      .then((value) => this.processPayload<TEntity[]>(value))
+      .catch((error) => {
+        console.error('Error in getAll:', error)
+        throw error
+      })
+  }
+
+  getByFilter(filter: string): Promise<TEntity[]> {
+    return this.get<TEntity[]>(`${this.baseUrl}/${filter}`, true)
+      .then((value) => this.processPayload<TEntity[]>(value))
+      .catch((error) => {
+        console.error('Error in getByFilter:', error)
+        throw error
+      })
+  }
+
+  add(entity: TEntity): Promise<boolean> {
+    return this.post<boolean>(`${this.baseUrl}`, entity, true)
+      .then((value) => this.processPayload<boolean>(value))
+      .catch((error) => {
+        console.error('Error in add:', error)
+        throw error
+      })
+  }
+
+  update(entity: TEntity): Promise<boolean> {
+    return this.put<boolean>(`${this.baseUrl}`, entity, true)
+      .then((value) => this.processPayload<boolean>(value))
+      .catch((error) => {
+        console.error('Error in update:', error)
+        throw error
+      })
+  }
+
+  remove(id: string): Promise<boolean> {
+    return this.delete<boolean>(`${this.baseUrl}/${id}`, true)
+      .then((value) => this.processPayload<boolean>(value))
+      .catch((error) => {
+        console.error('Error in remove:', error)
+        throw error
+      })
+  }
+}
