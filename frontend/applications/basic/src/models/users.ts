36e0963f59b75156fbf01a96a85d5a4c931c7331
H.Muhammad Kamran <hmuhdkamran@gmail.com>
2025-02-07 09:43:22 +0500

    Revert "Add eventlistner"

--- frontend/applications/basic/src/models/users.ts
+++
@@ -1,9 +0,0 @@
-export interface IUser {
-    UserId: string;
-    Username: string;
-    DisplayName: string;
-    Language: string;
-    Password: string;
-    Salt: string;
-    StatusId: string;
-  }
