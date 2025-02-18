3110468f6499c5e13960ff5c9d7c47fccb89c07c
Muhammad Kamran <hmuhdkamran@gmail.com>
2025-02-18 15:34:14 +0500

    add filter by in generic store

--- frontend/libraries/library/src/store/generic-store.ts
+++ frontend/libraries/library/src/store/generic-store.ts
@@ -1,65 +1,66 @@
 // ~/stores/generic.ts
-import { RepositoryService } from '@/service';
-import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { RepositoryService } from '@/service'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
 export function createGenericPiniaStore<T extends Record<string, any>, K extends keyof T>(
-    storeId: string,
-    apiPath: string,
-    primaryKey: K
+  storeId: string,
+  apiPath: string,
+  primaryKey: K,
 ) {
-    return defineStore(storeId, () => {
-        const service = new RepositoryService<T>(apiPath);
-        const items = ref<Array<T>>([]);
-        const item = ref<T>();
-        const isLoading = ref(false);
-        const shouldUpdate = ref(false);
+  return defineStore(storeId, () => {
+    const service = new RepositoryService<T>(apiPath)
+    const items = ref<Array<T>>([])
+    const item = ref<T>()
+    const isLoading = ref(false)
+    const shouldUpdate = ref(false)
 
-        async function getItems() {
-            isLoading.value = true;
-            try {
-                const response = await service.getAll();
-                items.value = response as Array<T>;
-            } catch (err) {
-                console.error(`Error: ${err}`);
-            } finally {
-                isLoading.value = false;
-            }
-        }
+    async function getItems(filter: string = '') {
+      isLoading.value = true
+      try {
+        const response =
+          filter.length > 0 ? await service.getByFilter(filter) : await service.getAll()
+        items.value = response as Array<T>
+      } catch (err) {
+        console.error(`Error: ${err}`)
+      } finally {
+        isLoading.value = false
+      }
+    }
 
-        async function createOrUpdateItem(itemToSave: T) {
-            isLoading.value = true;
-            try {
-                await shouldUpdate.value ? service.update(itemToSave) : service.add(itemToSave);
-                await getItems();
-            } catch (err) {
-                console.error(`Error: ${err}`);
-            } finally {
-                isLoading.value = false;
-            }
-        }
+    async function createOrUpdateItem(itemToSave: T) {
+      isLoading.value = true
+      try {
+        ;(await shouldUpdate.value) ? service.update(itemToSave) : service.add(itemToSave)
+        await getItems()
+      } catch (err) {
+        console.error(`Error: ${err}`)
+      } finally {
+        isLoading.value = false
+      }
+    }
 
-        async function deleteItem(itemToDelete: T) {
-            isLoading.value = true;
-            try {
-                const primaryKeyValue = itemToDelete[primaryKey];
-                await service.remove(primaryKeyValue);
-                await getItems();
-            } catch (err) {
-                console.error(`Error: ${err}`);
-            } finally {
-                isLoading.value = false;
-            }
-        }
+    async function deleteItem(itemToDelete: T) {
+      isLoading.value = true
+      try {
+        const primaryKeyValue = itemToDelete[primaryKey]
+        await service.remove(primaryKeyValue)
+        await getItems()
+      } catch (err) {
+        console.error(`Error: ${err}`)
+      } finally {
+        isLoading.value = false
+      }
+    }
 
-        return {
-            item,
-            items,
-            isLoading,
-            shouldUpdate,
-            getItems,
-            createOrUpdateItem,
-            deleteItem,
-        };
-    });
+    return {
+      item,
+      items,
+      isLoading,
+      shouldUpdate,
+      getItems,
+      createOrUpdateItem,
+      deleteItem,
+    }
+  })
 }
