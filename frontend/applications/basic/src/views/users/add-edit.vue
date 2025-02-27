01e41a6b19735f040a6f15c5233ca8b3aa83dd28
SaniyaSaeed <sania.saeedsefam@gmail.com>
2025-02-27 18:00:49 +0500

    fix minor

--- frontend/applications/basic/src/views/users/add-edit.vue
+++ frontend/applications/basic/src/views/users/add-edit.vue
@@ -182,7 +182,7 @@ const close = () => {
                         <h3 class="text-sm font-medium text-gray-700 py-3 sticky top-0 bg-white z-10">Roles:</h3>
                         <ul role="listbox" aria-label="role lists" class="grid grid-cols-2 gap-2">
                             <li v-for="item in roleStore.items" :key="item.RoleId" tabindex="-1" role="option">
-                                <FormCheckBox :id="item.RoleId" v-model="selectedRoles" :label="item.FullName" @update:modelValue="toggleRole(item.RoleId)"
+                                <FormCheckBox :id="item.RoleId" v-model="selectedRoles" :label="item.FullName"
                                     :value="item.RoleId" :icon="null"/>
                             </li>
                         </ul>
@@ -192,7 +192,7 @@ const close = () => {
                         <h3 class="text-sm font-medium text-gray-700 py-3 sticky top-0 bg-white z-10">Products:</h3>
                         <ul role="listbox" aria-label="product lists" class="grid grid-cols-1 gap-2">
                             <li v-for="item in productStore.items" :key="item.ProductId" tabindex="-1" role="option">
-                                <FormCheckBox :id="item.ProductId" v-model="selectedProducts" :label="item.FullName" @update:modelValue="toggleProduct(item.ProductId)"
+                                <FormCheckBox :id="item.ProductId" v-model="selectedProducts" :label="item.FullName"
                                     :value="item.ProductId" :icon="item.Icon" />
                             </li>
                         </ul>
